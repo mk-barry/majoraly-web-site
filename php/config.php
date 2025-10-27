2cb13b34b7c4afa9c83ec4ca4f0f6c15c6cbd793
mk-barry <mbatsbarry5@gmail.com>
2025-10-27 22:30:46 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 // Configuration - update these values to match your environment.
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'Majoraly');
+define('DB_HOST', 'interchange.proxy.rlwy.net:33921');
+define('DB_NAME', 'railway');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'UndWgwxnOvruVsiYQAYEfcFOQYvZhrJH');
 define('DB_CHAR', 'utf8mb4');
