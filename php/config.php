e0589c78d8c38ae7d60440f6c8b4f032691c5f91
mk-barry <mbatsbarry5@gmail.com>
2025-10-27 23:03:03 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 // Configuration - update these values to match your environment.
-define('DB_HOST', 'getenv("MYSQLHOST")');
-define('DB_NAME', 'getenv("MYSQLDATABASE")');
-define('DB_USER', 'getenv("MYSQLUSER")');
-define('DB_PASS', 'getenv("MYSQLPASSWORD")');
+define('DB_HOST', getenv("MYSQLHOST"));
+define('DB_NAME', getenv("MYSQLDATABASE"));
+define('DB_USER', getenv("MYSQLUSER"));
+define('DB_PASS', getenv("MYSQLPASSWORD"));
 define('DB_CHAR', 'utf8mb4');
