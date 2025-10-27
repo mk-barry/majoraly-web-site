203858ba9912d9636b375c294d083d58a649dfa7
mk-barry <mbatsbarry5@gmail.com>
2025-10-27 22:41:56 +0100

    Update connexion.php

--- php/connexion.php
+++ php/connexion.php
@@ -1,7 +1,7 @@
 <?php
-$serveur = "localhost";      // généralement localhost en local
+$serveur = "interchange.proxy.rlwy.net:33921";      // généralement localhost en local
 $utilisateur = "root";       // ton nom d'utilisateur MySQL
-$motdepasse = "";            // ton mot de passe MySQL (souvent vide en local)
+$motdepasse = "UndWgwxnOvruVsiYQAYEfcFOQYvZhrJH";            // ton mot de passe MySQL (souvent vide en local)
 $base = "majoraly";        // le nom de ta base de données
 
 // Connexion à MySQL
@@ -9,9 +9,10 @@ $conn = new mysqli($serveur, $utilisateur, $motdepasse, $base);
 
 // Vérifier si la connexion a échoué
 if ($conn->connect_error) {
-    die("❌ Échec de la connexion à la base de données : " . $conn->connect_error);
+    die("Échec de la connexion à la base de données : " . $conn->connect_error);
 }
 
 // Optionnel : définir l'encodage en UTF-8 pour les caractères spéciaux
 $conn->set_charset("utf8");
+
 ?>
